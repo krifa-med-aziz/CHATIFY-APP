d86c57108532c1c66b15a7db2e826417a9546279
Med Aziz Krifa <krifaaziz10@gmail.com>
2026-02-10 23:12:39 +0100

    feat: add routing and authentication features with new pages and Zustand store

---
+++ Frontend/src/store/useAuthStore.js
@@ -0,0 +1,13 @@
+import { truncate } from "fs/promises";
+import { create } from "zustand";
+
+export const useAuthStore = create((set) => ({
+  authUser: { name: "Aziz", _id: "123", age: 20 },
+  isLoggedIn: false,
+  isLoading: false,
+
+  login: () => {
+    console.log("We just logged in");
+    set({ isLoggedIn: true, isLoading: true });
+  },
+}));
